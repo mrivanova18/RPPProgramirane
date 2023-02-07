b710f652cdff4e94e06f8ecfdd54255e483651bf
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2023-02-07 12:55:52 +0200

    add file

---
+++ masivi/masivi/OptionsManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masivi
+{
+    public static class OptionsManager
+    {
+        public static int CountLess100(int[] arr)
+        {
+            return arr.Count(x => x < 100);
+        }
+        public static int MaxNum(int[] arr)
+        {
+            return arr.Max();
+        }
+        public static void EvenNum(int[] arr)
+        {
+            var result = arr.Where(x => x % 2 == 0).ToArray();
+            Console.WriteLine(string.Join(" ",result));
+        }
+        public static int SumEven(int[] arr)
+        {
+            return arr.Where(x=>x%2==0).Sum();
+        }
+        public static int CountEven(int[] arr)
+        {
+            return arr.Where(x => x % 2 == 0).Count();
+        }
+        public static void ReverseArr(int[] arr)
+        {
+            var result = arr.Reverse().ToArray();
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        public static void PrintArr(int[] arr)
+        {
+            Console.WriteLine(string.Join(", ",arr));
+        }
+    }
+}
---
+++ masivi/masivi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace masivi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(OptionsManager.CountLess100(arr));
+            Console.WriteLine(OptionsManager.MaxNum(arr));
+            OptionsManager.EvenNum(arr);
+            Console.WriteLine(OptionsManager.SumEven(arr));
+            Console.WriteLine(OptionsManager.CountEven(arr));
+            OptionsManager.ReverseArr(arr);
+        }
+    }
+}
