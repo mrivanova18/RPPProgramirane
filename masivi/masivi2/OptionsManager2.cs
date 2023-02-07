1daebd0e28009e679e01f17179a517f29a4f6f51
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2023-02-07 13:29:09 +0200

    add new file

---
+++ masivi/masivi2/OptionsManager2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masivi2
+{
+    public static class OptionsManager2
+    {
+        public static void ReverseArr(string[] words)
+        {
+            var result = words.Reverse().ToArray();
+            Console.WriteLine(string.Join(", ", result));
+        }
+        public static void Distinct(string[] words)
+        {
+            var result = words.Distinct();
+            
+        }
+        public static void Replace(string[] words,string word)
+        {
+            
+        }
+    }
+}
---
+++ masivi/masivi2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace masivi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+
+        }
+    }
+}
