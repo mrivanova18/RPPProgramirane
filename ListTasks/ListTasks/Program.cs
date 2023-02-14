c621bbcd341236e972ea4a616510db06705f228c
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2023-02-14 12:56:06 +0200

    upload first task

--- ListTasks/ListTasks/Program.cs
+++ ListTasks/ListTasks/Program.cs
@@ -9,7 +9,9 @@ namespace ListTasks
         static void Main(string[] args)
         {
             List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
-
+            int last = nums[nums.Count - 1];
+            nums.RemoveAll(x=>x==last);
+            Console.WriteLine(string.Join(" ",nums));
         }
     }
 }
