6710be268afd2b1cca677d487068ba6290a58392
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2023-02-14 13:30:24 +0200

    add new project

---
+++ ListTasks/ListMerge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> result = new List<int>();
+            List<string> input = Console.ReadLine().Split("|",StringSplitOptions.RemoveEmptyEntries).ToList();
+            input.Reverse();
+            for (int i = 0; i < input.Count; i++)
+            {
+                int[] nums = input[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    result.Add(nums[j]);
+                }
+            }
+            Console.WriteLine(string.Join(" ",result));
+        }
+    }
+}
