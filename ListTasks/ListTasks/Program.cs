220117ffd595a0cf46bbed52630c4196129cc384
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2023-02-14 12:34:32 +0200

    add project

---
+++ ListTasks/ListTasks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+        }
+    }
+}
