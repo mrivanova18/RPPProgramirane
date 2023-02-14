8caf020925f28abf4f95c84cb6ff27912c71ea81
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2023-02-14 13:13:39 +0200

    add new project

---
+++ ListTasks/NegativeNum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NegativeNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            nums.RemoveAll(x => x < 0);
+            if (nums.Count==0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                nums.Reverse();
+                Console.WriteLine(string.Join(" ",nums));
+            }
+        }
+    }
+}
