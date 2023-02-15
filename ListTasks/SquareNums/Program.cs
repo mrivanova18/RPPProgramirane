1aef8961f8a2e3c30dad9ac0271b4e8b9081a21d
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2023-02-15 09:27:13 +0200

    add project

---
+++ ListTasks/SquareNums/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SquareNums
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {          
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            for (int i = 0; i < nums.Count; i++)
+            {
+                List<int> result = new List<int>();
+                nums.Where(x => Math.Sqrt(x) == (int)Math.Sqrt(x));
+                result.Add(nums[i]);
+                Console.WriteLine(string.Join(" ", result));
+            }
+            /*int num = int.Parse(Console.ReadLine());
+            if (Math.Sqrt(num) == (int)Math.Sqrt(num))
+            {
+                nums.Add(num);
+                List<int> result = new List<int>();
+                result.Add(num);
+                Console.WriteLine(string.Join(" ", result));
+            }
+            else
+            {
+                nums.Add(num);
+            }*/                                      
+        }        
+    }
+}
