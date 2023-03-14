33e62733b0c957c3ca96f34e2e0b1c85c3b61f6e
mrivanova18 <m.r.ivanova2019@pmg-pernik.com>
2023-03-14 12:45:57 +0200

    add project

---
+++ Dictionary/Minyor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Minyor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            var resources = Console.ReadLine();           
+            while (resources!="stop")
+            {
+                var quantity = int.Parse(Console.ReadLine());
+                if (dict.ContainsKey(resources))
+                {
+                    dict[resources] += quantity;
+                }
+                else
+                {
+                    dict.Add(resources, quantity);
+                }
+            }
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key}->{item.Value}");
+            }
+            
+        }
+    }
+}
